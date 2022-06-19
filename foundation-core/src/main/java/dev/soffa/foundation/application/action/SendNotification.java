f1e087d51f789bdba3ec59523a00dcd2b05261ff
Titus Gahissy <titus@gahissy.com>
2022-06-19 18:36:12 +0100

    Release 0.13.2

---
+++ foundation-core/src/main/java/dev/soffa/foundation/application/action/SendNotification.java
@@ -0,0 +1,6 @@
+package dev.soffa.foundation.application.action;
+
+import dev.soffa.foundation.core.Operation;
+import dev.soffa.foundation.model.TemplateMessage;
+
+public interface SendNotification extends Operation<TemplateMessage, Void> {}
