bc73d5bb72c58b5fa570f866c49bc90177f0dba1
Titus Gahissy <titus@gahissy.com>
2022-06-26 14:55:54 +0100

    Throw validation error when operation is not an event handler

--- foundation-api/src/main/java/dev/soffa/foundation/commons/validation/ValidationResult.java
+++ foundation-api/src/main/java/dev/soffa/foundation/commons/validation/ValidationResult.java
@@ -1,11 +1,13 @@
 package dev.soffa.foundation.commons.validation;
 
+import dev.soffa.foundation.error.ValidationException;
+
 import java.util.Map;
 
 public interface ValidationResult {
 
     Map<String,String> getErrors();
-
     boolean hasErrors();
+    void thowAnyError() throws ValidationException;
 
 }
