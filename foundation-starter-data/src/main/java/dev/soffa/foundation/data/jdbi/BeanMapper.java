3d0ea18f0da454446f62c3d82320d729f6574161
Titus Gahissy <titus@gahissy.com>
2022-06-28 00:42:17 +0100

    Jdbi BeanMapper rollback

--- foundation-starter-data/src/main/java/dev/soffa/foundation/data/jdbi/BeanMapper.java
+++ foundation-starter-data/src/main/java/dev/soffa/foundation/data/jdbi/BeanMapper.java
@@ -3,10 +3,10 @@ package dev.soffa.foundation.data.jdbi;
 import dev.soffa.foundation.commons.Mappers;
 import dev.soffa.foundation.data.EntityInfo;
 import lombok.SneakyThrows;
-import org.apache.commons.beanutils.ConvertUtils;
 import org.jdbi.v3.core.mapper.RowMapper;
 import org.jdbi.v3.core.statement.StatementContext;
 
+import java.sql.Clob;
 import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Map;
@@ -30,20 +30,14 @@ public final class BeanMapper<T> implements RowMapper<T> {
         for (Map.Entry<String, String> e : entityInfo.getPropertiesToColumnsMapping().entrySet()) {
             String col = e.getValue();
             Object value = rs.getObject(col);
-            String prop = e.getKey();
-            Class<?> target = entityInfo.getPropertyType(prop);
 
             if (value == null) {
                 values.put(col, null);
                 continue;
             }
 
-            value = ConvertUtils.convert(value, target);
-            values.put(col, value);
-            values.put(prop, value);
-
-            /*
-
+            String prop = e.getKey();
+            Class<?> target = entityInfo.getPropertyType(prop);
             if (value instanceof Clob) {
                 value = rs.getString(col);
             }
@@ -59,8 +53,7 @@ public final class BeanMapper<T> implements RowMapper<T> {
                     value = Mappers.JSON_FULLACCESS_SNAKE.deserializeMap(value.toString());
                 }
             }
-
-             */
+            values.put(col, value);
         }
         return Mappers.JSON_FULLACCESS_SNAKE.convert(values, entityInfo.getEntityClass());
     }
