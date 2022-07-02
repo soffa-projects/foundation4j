661dfef766ea67602f10437aac23107d6288d024
Titus Gahissy <titus@gahissy.com>
2022-07-02 22:27:34 +0100

    Rollback karate-config

--- foundation-api/src/main/resources/karate-config.js
+++ foundation-api/src/main/resources/karate-config.js
@@ -1,7 +1,7 @@
 function fn() {
     karate.configure('connectTimeout', 5000);
     karate.configure('readTimeout', 5000);
-    karate.configure('retry', {count: 20, interval: 1000});
+    karate.configure('retry', {count: 5, interval: 500});
     var config = JSON.parse(karate.properties['config'] || "{}")
     if (!config.baseUrl) {
         config.baseUrl = 'http://mockmvc';
